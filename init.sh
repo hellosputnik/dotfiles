6f2829d4b775c245b3edc08d094aaeaf2ac1a40b
hellosputnik <hallosputnik@gmail.com>
2017-01-04 16:58:51 -0800

    Update init script
    The installation of the Vim configuration and its plugins has been greatly
    simplified. Copy the vim folder to ~/.vim and done!

--- init.sh
+++ init.sh
@@ -1,9 +1,3 @@
-# Configure Vim
-cp vim/.vimrc ~/.vimrc
-
-# Install Pathogen for Vim
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-cp vim/autoload ~/.vim/autoload
-cp vim/bundle ~/.vim/bundle
-# curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim > ~/.vim/autoload/pathogen.vim
+# Install Vim configurations and plugins (e.g. vimrc, pathogen)
+cp -R vim ~/.vim
 
