388aa184c38bcb63cbf567ce3241b15b053f1949
hellosputnik <hallosputnik@gmail.com>
2017-01-05 15:44:44 -0800

    Add bash configurations

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+# Install bash configuraitons
+for $file in bash{_profile,_prompt,rc}; do
+    cp bash/$file ~/.$file
+done;
+
 # Install tmux configurations
 cp tmux/tmux.conf ~/.tmux.conf
 
@@ -6,5 +11,4 @@ cp vim/vimrc ~/.vimrc
 
 # Install Vim plugins
 cp -fR vim/vim/ ~/.vim
-
 
