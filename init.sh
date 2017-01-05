3d6ef852a14daa8e3a30871e99149af489215d17
hellosputnik <hallosputnik@gmail.com>
2017-01-04 16:49:12 -0800

    Add pathogen to Vim

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
 # Configure Vim
 cp vim/.vimrc ~/.vimrc
+
+# Install Pathogen for Vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+cp vim/autoload ~/.vim/autoload
+cp vim/bundle ~/.vim/bundle
+# curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim > ~/.vim/autoload/pathogen.vim
+
