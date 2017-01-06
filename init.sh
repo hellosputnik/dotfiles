42983ea38ac22227cc93f26ecc64e08a125e70b4
Michael Nguyen <michael-nguyen@apple.com>
2017-01-05 17:12:44 -0800

    Add initial bash profile, prompt, and aliases configurations

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Install bash configurations (bash_profile, bash_prompt, bashrc)
+echo {_profile,_prompt,rc} | xargs -n 1 -I{} cp bash/bash{} ~/.bash{}
+
 # Install tmux configurations
 cp tmux/tmux.conf ~/.tmux.conf
 
@@ -6,5 +11,4 @@ cp vim/vimrc ~/.vimrc
 
 # Install Vim plugins
 cp -fR vim/vim/ ~/.vim
-
 
