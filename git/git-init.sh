a20f44305dcd479c3d44318ec0f1ec4127796347
Michael Nguyen <michael-nguyen@apple.com>
2026-05-02 01:43:40 -0700

    Optimize git-init utility script options

--- git/git-init.sh
+++ git/git-init.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+# Write to ~/.gitconfig.local so identity stays out of the dotfiles-tracked
+# main gitconfig. The main gitconfig includes this file via [include].
+LOCAL_CONFIG="$HOME/.gitconfig.local"
+
 echo "Please enter in your git username: "
-read name
-git config --global user.name "$name"
+read -r name
+git config --file "$LOCAL_CONFIG" user.name "$name"
 
 echo "Please enter in your git email address: "
-read email
-git config --global user.email "$email"
+read -r email
+git config --file "$LOCAL_CONFIG" user.email "$email"
 
 echo "Please enter in your preferred editor (e.g. vim, emacs, nano): "
-read editor
-git config --global core.editor "$editor"
-
+read -r editor
+git config --file "$LOCAL_CONFIG" core.editor "$editor"
